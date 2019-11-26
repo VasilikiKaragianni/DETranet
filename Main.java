088d194f0fc7a7c6058ae2ce2b13bc9a6c4bdb8f
MarinosFokas <56540308+MarinosFokas@users.noreply.github.com>
2019-11-27 00:50:07 +0200

    Add files via upload
    Ολοκλήρωσα και παραθετω τη κλάση Main.java(το εκτελεσιμο κομματι).

--- Main.java
+++ Main.java
@@ -29,7 +29,7 @@ public class Main {
 		}while(flag);	
 		int startLeaves = 31;
 		int id=Employee.count;
-		String inputPassword;
+		String inputPassword="";
 		if(select==1) {
 			int selectDep=0;
 			flag=true;
@@ -84,27 +84,38 @@ public class Main {
 			}while(flag);
 			switch (selectDep) {
 			case 1:
-				Manager manager = new Manager (inputFullname, id, "Manager" ,inputEmail,2000,firstday,startLeaves,inputUsername,inputPassword,0); 
+				Manager manager = new Manager (inputFullname, id, "Manager" ,inputEmail,2000,firstday,startLeaves,inputUsername,inputPassword,0);
+				break;
 			case 2:
-				
+				LoanManager lnmngr= new LoanManger(inputFullname, id, "Loan Manager" ,inputEmail,1700,firstday,startLeaves,inputUsername,inputPassword,0);
+				break;
 			case 3:
-				
+				DepositManager dpmngr= new DepositManager(inputFullname, id, "Deposit Manager" ,inputEmail,1700,firstday,startLeaves,inputUsername,inputPassword,0);
+				break;
 			case 4:
-				
+				PrivateCustomerManager prcumn= new PrivateCustomerManager(inputFullname, id, "Private Customer Manager" ,inputEmail,1400,firstday,startLeaves,inputUsername,inputPassword,0);
+				break;
 			case 5:
-				
+				Teller teller = new Teller(inputFullname, id, "Teller" ,inputEmail,1300,firstday,startLeaves,inputUsername,inputPassword,0);
+				break;
 			case 6:
-				
+				PrivateCustomerManagerGOLD prcumngold = new PrivateCustomerManager(inputFullname, id, "Private costumer manager GOLD" ,inputEmail,1300,firstday,startLeaves,inputUsername,inputPassword,0); 
+				break;
 			case 7:
-				
+				PrivateCustomerManagerDelays prcumndel = new PrivateCustomerManagerDelay (inputFullname, id, "Private customer manager delays" ,inputEmail,1300,firstday,startLeaves,inputUsername,inputPassword,0);
+				break;
 			case 8:
-				
+				BusinessServiceManager bsnsermn = new BusinessServiceManager(inputFullname, id, "Business service manager" ,inputEmail,1300,firstday,startLeaves,inputUsername,inputPassword,0);
+				break;
 			case 9:
-				
+				CustumerServiceManager cusermn = new CustumerServiceManager(inputFullname, id, "Customer service manager" ,inputEmail,1300,firstday,startLeaves,inputUsername,inputPassword,0);
+				break;
 			case 10:
-				
+				CustomerServiceManagerGOLD cusermngold = new CustomerServiceManagerGOLD(inputFullname, id, "Customer service manager GOLD" ,inputEmail,1300,firstday,startLeaves,inputUsername,inputPassword,0);
+				break;
 			case 11:
-				
+				CustomerServiceManagerDelays cusermndel = new CustomerServiceManagerDelays(inputFullname, id, "Customer service manager delays" ,inputEmail,1300,firstday,startLeaves,inputUsername,inputPassword,0);
+				break;
 			}		
 		}else {
 			boolean successLogIn = true;
@@ -125,10 +136,10 @@ public class Main {
 					if (ans.equals("Y")) successLogIn=false;
 				} else {
 					successLogIn=false;
-					Employee.logIn(inputUsername, inputPassword).getMenu();
+					Employee.logIn(inputUsername, logInInputPassword).getMenu();
 				}
 			}while (successLogIn);	
 			main(args);
 		}
 	}
-}	
+}
