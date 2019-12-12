8a22fe7cbf19fcfcb6049f9b695bb8b6e108f4c4
ivogkas <56538610+ivogkas@users.noreply.github.com>
2019-12-12 23:25:20 +0200

    Προσθηκη σχολιων

--- DepositManager.java
+++ DepositManager.java
@@ -2,77 +2,82 @@ package detranet;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
+/* Class for deposit Manager */
 public class DepositManager extends Employee {
 	
 	private static String pCMGoals;
 	private static String tellerGoals;
-	private static String pCMGoldGoals;
+	private static String pCMVipGoals;
 	private static String pCMDelaysGoals;
 	
 	public DepositManager(String fullname, int idEmployee, String department, String email, double salary,
 			String firstDate, int leaves, String username, String password, double overall) {
 		super(fullname, idEmployee, department, email, salary, firstDate, leaves, username, password, overall);
 	}
-
+	
+	/* Display the goals for the deposit department set by the manager */
 	@Override
 	public void goals() {
-		Scanner sc = new Scanner(System.in);
-		if (Manager.getDepositGoals()==null) {
-			System.out.println("No available goals!");
+		if (Manager.getDepositGoals() == null) {
+			System.out.printf("No available goals!");
 		}else {
-			System.out.println("Department goals:\n" + Manager.getDepositGoals());
+			System.out.printf("Department goals:\n" + Manager.getDepositGoals());
 		}
-		String goBack = sc.next();
-		getMenu();	
 	}
 	
+	/* Method for goals assignment for employees of deposit department */
 	public void setGoals() {
-		for(;;) {
-			boolean flag=true;
-			Scanner sc = new Scanner (System.in);
-			int select=0;;
+		Scanner sc = new Scanner (System.in);
+		boolean endOfProcedure = true;
+		do {
+			boolean flag = true;
+			int select = 0;
 			do {
 				try {
-					System.out.println("\nGoals sharing for depositors."
-							+ "\n1.Private costumer manager."
+					System.out.printf("\nGoals sharing for depositors."
+							+ "\n1.Private customer manager."
 							+ "\n2.Teller"
-							+ "\n3.Private costumer manager GOLD"
-							+ "\n4.Private costumer manager delays"
-							+ "\n5.Εnd of procedure");
-					select=sc.nextInt();
-					if (select>0 && select<6) {
-						flag=false;
+							+ "\n3.Private customer manager vip"
+							+ "\n4.Private customer manager delays"
+							+ "\n5.Εnd of procedure\n");
+					select = sc.nextInt();
+					if (select > 0 && select < 6) {
+						flag = false;
 					}else {
 						System.out.printf("Please insert an integer between 1-5.Try again...");
 						}
 					}
 				catch (InputMismatchException inputmismatchexception) {
-					System.err.printf("%nException%n: %s%n" , inputmismatchexception);
+					System.err.printf("%nException%n: %s%n", inputmismatchexception);
 					sc.nextLine();
 					System.out.printf("Please insert an integer between 1-5.Try again...");
 				}
 			}while (flag);
-			
 			switch (select) {
 			case 1:
-				pCMGoals = sc.nextLine();
+				System.out.printf("Private service manager goals:\n");
+				pCMGoals = sc.next();
 				break;
 			case 2:
-				tellerGoals = sc.nextLine();
+				System.out.printf("Teller goals:\n");
+				tellerGoals = sc.next();
 				break;
 			case 3:
-				pCMGoldGoals = sc.nextLine();
+				System.out.printf("Private customer manager Vip goals:\n");
+				pCMVipGoals = sc.next();
 				break;
 			case 4:
-				pCMDelaysGoals = sc.nextLine();
+				System.out.printf("Private customer manager delays goals:\n");
+				pCMDelaysGoals = sc.next();
 				break;
 			case 5:
-				getMenu();
+				endOfProcedure = false;
 				break;
-				}
 			}
+		} while (endOfProcedure);
 	}
+	
+	/* Methods return goals for employees of deposit department */
 	public static String getpCMGoals() {
 		return pCMGoals;
 	}
@@ -80,158 +85,115 @@ public class DepositManager extends Employee {
 		return tellerGoals;
 	}
 	public static String getpCMGoldGoals() {
-		return pCMGoldGoals;
+		return pCMVipGoals;
 	}
 	public static String getpCMDelaysGoals() {
 		return pCMDelaysGoals;
 	}
 	
 	
-	
+	/* Method compute bonus for deposit manager 
+	 * based on the average overall of the employees of the deposit department */
 	@Override
 	public double computeBonus() {
-		int nmbrOfEmployees=0;
-		double sumOverall=0;
+		int nmbrOfEmployees = 0;
+		double sumOverall = 0;
 		double rate = 0;
-		double bonus=0;
-		for(int i=0; i<employees.size(); i++) {
+		double bonus = 0;
+		/* Find deposit employees and sum overalls */
+		for(int i=0; i < employees.size(); i++) {
 			Employee emp = employees.get(i);
-			if (emp.getDepartment()=="PrivateCostumerManager" 
-					|| emp.getDepartment()=="Teller" 
-					|| emp.getDepartment()=="PrivateCostumerManagerGold" 
-					|| emp.getDepartment()=="PrivateCostumerManagerDelays") {
-				nmbrOfEmployees =+ 1;
-				sumOverall =+ overalls.get(i);
-			}
-		}	
-		for(int j=0; j<employees.size(); j++) {
-			Employee empDM = employees.get(j);
-			if(empDM.getDepartment()=="DepositManager") {
-				rate=sumOverall/nmbrOfEmployees;
-				empDM.setOverall(rate);
-				idEmployees.add(empDM.getIdEmployee());
-				overalls.add(rate);
-			}
-		}	 
-		if(rate>75) {
-			bonus = rate * 3;
-		}
-		return bonus;
-	}
-	@Override
-	public void leaves() {
-		Scanner sc = new Scanner(System.in);
-		int select=0;
-		boolean flag=true;
-		do {
-			try {
-				System.out.println("1.Display remaining leaves"
-									+ "\n2.Request for leave");
-			select=sc.nextInt();
-			if(select==1 ||select==2) {
-				flag=false;	
-			}else {
-				System.out.println("Please enter 1 for display or 2 for register.Try again...");
+			if (emp.getDepartment() == "Private Customer Manager" 
+					|| emp.getDepartment() == "Teller" 
+					|| emp.getDepartment() == "Private Customer Manager Vip" 
+					|| emp.getDepartment() == "Private Customer Manager Delays") {
+				int idCurrEmployee = emp.getIdEmployee();
+				nmbrOfEmployees += 1;
+				for (int j = 0; j < idEmployees.size(); j++ ) {			/* The positions of employees are not  */
+					if (idCurrEmployee == idEmployees.get(j)) {			/* the same in lists employees and idEmployees */
+						sumOverall += overalls.get(j);
+						break;
+					}
 				}
 			}
-			catch (InputMismatchException inputmismatchexception) {
-				System.err.printf("%nException%n: %s%n" , inputmismatchexception);
-				sc.nextLine();
-				System.out.println("Please enter 1 for display or 2 for register.Try again...");
-			}
-			
-		}while(flag);
-		int days=0;
-		if(select==1) {
-			System.out.println("Remaining days of leave: " + this.getLeaves() + "!");
-		}else if (select==2) {
-			if (this.getLeaves()>0) {
-				boolean newRequest=true;
-				do {
-					flag=true;
-					do {
-						try {
-								System.out.printf("\nNumber of days needed: ");
-								days = sc.nextInt();
-								flag=false;
-						}catch (InputMismatchException inputmismatchexception) {
-							System.err.printf("%nException%n: %s%n" , inputmismatchexception);
-							sc.nextLine();
-							System.out.printf("Please enter an integer.Try again...");
-						}
-					}while(flag);
-					if (days>this.getLeaves()) {
-						System.out.println("\nSorry.Only " + this.getLeaves() + " remaining days of leave!");
-						String ans;
-						do {
-							System.out.println("\nDo you want to apply again?;(Y/N)");
-							ans=sc.next();
-							ans=ans.toUpperCase();
-						}while(!ans.equals("Y")  && !ans.equals("N"));
-						if(ans.equals("N")) newRequest=false;	
-					}else {
-						newRequest=false;
-						this.setLeaves(this.getLeaves()-days);
-						System.out.println("Υour request has been accepted!"
-								+ "\nRemaining days of leave: " + this.getLeaves());
-					}
-				}while (newRequest);
-			}else {
-				System.out.println("\nThere are not remaining days of leave!");
-			}
 		}
-		String goBack = sc.next();
-		getMenu();
+		rate = sumOverall / nmbrOfEmployees;
+		if(rate > 75) {
+			bonus = rate * 3;
+		}	
+		return bonus;
 	}
 	
+	 /* Add rate of deposit manager */
+	public void evaluationDepositManager() {
+		double rate = (computeBonus()/3);
+		for(int j = 0; j < employees.size(); j++) {
+			Employee empLM = employees.get(j);
+			if(empLM.getDepartment() == "Deposit Manager") {
+				empLM.setOverall(rate);
+				idEmployees.add(empLM.getIdEmployee());
+				overalls.add(rate);
+				break;
+			}
+		}	
+	}
+	
+	public double finalSalary() {
+		return (this.getSalary() + computeBonus());
+	}
+	
+	/* Display the menu for deposit manager */
 	@Override
 	public void getMenu() {
 		Scanner sc = new Scanner (System.in);
-		int select=0;
-		boolean flag=true;
-		do {
-			try  {
-				System.out.println("\n Welcome to the Deposit Manager menu!"
-						+ "\n1.Display department goals"
-						+ "\n2.Goals sharing"
-						+ "\n3.Leaves"
-						+ "\n4.Compute BONUS"
-						+ "\n5.News"
-						+ "\n6.Log Out");
-				select=sc.nextInt();
-				if (select>0 && select<6) {
-					flag=false;
-				}else {
-					System.out.printf("Please insert an integer between 1-5.Try again...");
+		for(;;) {
+			int select = 0;
+			boolean flag = true;
+			do {
+				try {
+					System.out.printf("\nWelcome to the Deposit Manager menu!"
+							+ "\n1.Display department goals"
+							+ "\n2.Goals sharing"
+							+ "\n3.Leaves"
+							+ "\n4.Compute BONUS"
+							+ "\n5.News"
+							+ "\n6.Log Out\n");
+					select = sc.nextInt();
+					if (select > 0 && select < 7) {
+						flag = false;
+					}else {
+						System.out.printf("Please insert an integer between 1-6.Try again...");
 					}
+				}	
+				catch (InputMismatchException inputmismatchexception) {
+					System.err.printf("%nException%n: %s%n", inputmismatchexception);
+					sc.nextLine();
+					System.out.printf("Please insert an integer between 1-6.Try again...");
 				}
-			catch (InputMismatchException inputmismatchexception) {
-				System.err.printf("%nException%n: %s%n" , inputmismatchexception);
-				sc.nextLine();
-				System.out.printf("Please insert an integer between 1-5.Try again...");
+			}while(flag);
+			switch (select) {
+			case 1:
+				goals();
+				break;
+			case 2:
+				setGoals();
+				break;
+			case 3:
+				leaves();
+				break;
+			case 4:
+				evaluationDepositManager();
+				System.out.println("Goals achievment rate for deposit manager: " + (computeBonus()/3) + "%" + 
+						"\nBonus: " + computeBonus() + "$" + 
+						"\nFinal Salary: " + finalSalary() + "$");
+				break;
+			case 5:
+				Employee.getNews();
+				break;
+			case 6:
+				Main.main(null);
+				break;
 			}
-		}while (flag);
-		
-		switch (select) {
-		case 1:
-			goals();
-			break;
-		case 2:
-			setGoals();
-			break;
-		case 3:
-			leaves();
-			break;
-		case 4:
-			System.out.println("Goals achievment rate for depositors: " + this.getOverall() +
-					"\nBonus: " + computeBonus() + "$");
-			break;
-		case 5:
-			this.getNews();
-			break;
-		case 6:
-			Main.main(null);
-			break;
-		}
+		}	
 	}
 }
